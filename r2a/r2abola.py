16630ba32bb690c366dc9f5e28af41af1e1aada7
Gabriel Vasconcelos <gcvasconcelos98@gmail.com>
2021-10-24 17:29:34 -0300

    minor changes

--- r2a/r2abola.py
+++ r2a/r2abola.py
@@ -1,6 +1,6 @@
 # Teleinformática e Redes 2
 # Trabalho Final
-# Integrantes: 
+# Integrantes:
 ## Luis Filipe Campos Cardoso (19/0100079)
 ## Gabriel Vasconcelos (16/0120781)
 ## Alexander Matheus de Melo Lima (12/0108534)
@@ -12,7 +12,7 @@ from player.parser import *
 from base.configuration_parser import ConfigurationParser
 
 class R2ABOLA(IR2A):
-    
+
     def __init__(self, id):
         IR2A.__init__(self, id)
         self.segment_size = []          # S
@@ -22,7 +22,7 @@ class R2ABOLA(IR2A):
     # Auxiliary methods
     def log_utility_function(self, bitrate_index): # v
         # bitrate_index = m
-        return math.log(self.segment_size[bitrate_index]/self.segment_size[1]) 
+        return math.log(self.segment_size[bitrate_index]/self.segment_size[0])
 
     def optimal_solution(self,bitrate_index):
         # bitrate_index = m
@@ -45,7 +45,7 @@ class R2ABOLA(IR2A):
                 bitrate_index_selected = bitrate_index
                 optimal_solution_max = optimal_solution
 
-        return bitrate_index_selected           
+        return bitrate_index_selected
 
     # R2A methods
     def handle_xml_request(self, msg):
@@ -58,14 +58,14 @@ class R2ABOLA(IR2A):
 
         # Add quality to the video segment
         msg.add_quality_id(self.segment_size[quality])
-        
+
         # Send the msg to the bottom layer (ConnectionHandler)
         self.send_down(msg)
 
     def handle_xml_response(self, msg):
         # Get the payload of the mpd file
         self.parsed_mpd = parse_mpd(msg.get_payload())
-        
+
         # List qualities
         self.qi = self.parsed_mpd.get_qi()
         self.segment_size = self.qi
@@ -75,7 +75,7 @@ class R2ABOLA(IR2A):
 
         # Calculate the max value of the logarithmic utility function
         log_utility_max = self.log_utility_function(len(self.segment_size) - 1)
-        
+
         # Calculate gamma
         self.gamma = ((buffer_size_max - 1) * (self.log_utility_function(0) * self.segment_size[1] - self.log_utility_function(1) * self.segment_size[0]) - 2 * log_utility_max * (self.segment_size[1] - self.segment_size[0])) / (1 * (self.segment_size[1] - self.segment_size[0]) * (2 - buffer_size_max - 1))
 
@@ -88,7 +88,7 @@ class R2ABOLA(IR2A):
         print('Qualities: ', self.qi)
         print('Control Parameter (Performance weight): ', self.control_parameter)
         print('Gamma (Smoothness weight): ', self.gamma)
-        
+
         # Send the mesg to the upper layer (Player)
         self.send_up(msg)
 
